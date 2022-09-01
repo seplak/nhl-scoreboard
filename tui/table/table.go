fae6b8088a204efedcb237a8042873f0548d217b
seplak <88915100+seplak@users.noreply.github.com>
2022-08-31 20:58:38 -0400

    Use charmbracelet table (#4)
    * refactor: use charmbracelet table component
    
    * refactor: delete unused column keys

--- tui/table/table.go
+++ tui/table/table.go
@@ -4,14 +4,9 @@ import (
 	"strings"
 	"fmt"
 
-	"github.com/evertras/bubble-table/table"
+	"github.com/charmbracelet/bubbles/table"
 	"github.com/seplak/nhl-scoreboard/data"
 	tea "github.com/charmbracelet/bubbletea"
-)
-
-const (
-	columnKeyMatchup = "matchup"
-	columnKeyTime    = "time"
 )
 
 type Model struct {
@@ -24,15 +19,19 @@ func (m Model) NewModel(games []data.Game) Model {
 	model := Model{
 		games: games,
 	}
-	model.table = table.New(generateScheduleColumns()).WithRows(generateRowsFromData(model.games))
-
+	model.table = table.New(
+		table.WithColumns(generateScheduleColumns()),
+		table.WithRows(generateRowsFromData(model.games)),
+		table.WithFocused(true),
+		table.WithHeight(len(model.games) + 1),
+	)
 	return model
 }
 
 func generateScheduleColumns() []table.Column {
 	return []table.Column{
-		table.NewColumn(columnKeyMatchup, "Matchup", 50),
-		table.NewColumn(columnKeyTime, "Time", 10),
+		{Title: "Matchup", Width: 50},
+		{Title: "Time", Width: 10},
 	}
 }
 
@@ -51,18 +50,15 @@ func generateRowsFromData(games []data.Game) []table.Row {
 
 	if len(games) == 0 {
 		return []table.Row{
-			table.NewRow(table.RowData{
-				columnKeyMatchup: "No games today.",
-				columnKeyTime:    "N/A",
-			}),
+			{"No games today.", "N/A"},
 		}
 	}
 
 	for _, game := range games {
-		row := table.NewRow(table.RowData{
-			columnKeyMatchup: fmt.Sprintf("%s @ %s", game.AwayTeamName(), game.HomeTeamName()),
-			columnKeyTime:    game.GetGameDate(),
-		})
+		row := table.Row{
+			fmt.Sprintf("%s @ %s", game.AwayTeamName(), game.HomeTeamName()),
+			game.GetGameDate(),
+		}
 
 		rows = append(rows, row)
 	}
